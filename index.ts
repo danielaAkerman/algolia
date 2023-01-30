f31e010d2a3cf3053da8d59d5e1401b78cdbfe26
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-01-30 16:46:15 -0300

    geoloc

--- index.ts
+++ index.ts
@@ -6,16 +6,32 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-index
-  .saveObject({
-    objectID: "12",
-    nombre: "Cuaderno",
-    precio: 440,
-  })
-  .then((res) => {
-    console.log(res);
-  });
+// index
+//   .saveObject({
+//     objectID: "12",
+//     nombre: "Cuaderno",
+//     precio: 440,
+//     _geoloc: {
+//       lat: -31.43306,
+//       lng: -64.224994,
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
 
+// index.search("cuaderno").then((res) => {
+//   console.log(res.hits[0]);
+// });
+
+// index
+//   .search("", {
+//     aroundLatLng: "-31, -64",
+//     aroundRadius: 1000000 // metros
+//   })
+//   .then(({ hits }) => {
+//     console.log(hits);
+//   });
 
 // app.get("/test", async (req, res) => {
 //   res.json({});
